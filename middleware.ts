7965f7a99705c56adcce2edc53741970c998b767
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2023-02-24 11:29:49 +0900

    new cache api

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,21 @@
 import { NextMiddleware, NextResponse } from 'next/server'
 
 export const middleware: NextMiddleware = async (request, event) => {
-  const cache = await caches.open('cache:example')
+  // const cache = await caches.open('cache:example')
   const url = new URL('https://www.googleapis.com/identitytoolkit/v3/relyingparty/publicKeys')
-  let pubKeyRes = await cache.match(url)
-  if (!pubKeyRes) {
-    console.log('cache NOT hit')
-    pubKeyRes = await fetch(url)
-    event.waitUntil(cache.put(url, pubKeyRes))
-  } else {
-    console.log('cache hit')
-  }
+  // let pubKeyRes = await cache.match(url)
+  // if (!pubKeyRes) {
+  //   console.log('cache NOT hit')
+  //   pubKeyRes = await fetch(url)
+  //   event.waitUntil(cache.put(url, pubKeyRes))
+  // } else {
+  //   console.log('cache hit')
+  // }
+
+  const startTime = Date.now();
+  const pubLeuRes = await fetch(url, { next: { revalidate: 60 } })
+  const endTime = Date.now();
+  console.log(endTime - startTime, "ms");
 
   return NextResponse.next()
 }
